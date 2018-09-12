20352b5f00a0793c973da61f637a70231bd20072
merlx <merlx@lmerlxw7a.rpega.com>
2018-09-12 11:37:17 +0100

    Comment removed

--- dashboardlord/src/dashboardlord/listenv.java
+++ dashboardlord/src/dashboardlord/listenv.java
@@ -190,7 +190,7 @@ public class listenv extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 		doGet(request, response);
 	}
 
